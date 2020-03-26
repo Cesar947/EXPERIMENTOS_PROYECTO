154ecb0bb9e544b4b112c6ffb9caff5d40d64279
Cesar <u201710947@upc.edu.pe>
2020-03-26 00:56:36 -0500

    probando GetMapping()

--- EzDeal/API JAVA/src/main/java/com/myorg/ezdeal/controller/UsuarioController.java
+++ EzDeal/API JAVA/src/main/java/com/myorg/ezdeal/controller/UsuarioController.java
@@ -3,11 +3,13 @@ package com.myorg.ezdeal.controller;
 
 import com.myorg.ezdeal.models.Usuario;
 import com.myorg.ezdeal.service.UsuarioService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
+@Slf4j /*Para usar el comando log*/
 public class UsuarioController {
 
     private UsuarioService usuarioService;
@@ -26,6 +28,7 @@ public class UsuarioController {
 
     @GetMapping("/")
     public String HolaMundo(){
+        log.info("Ejecutando");
         return "Hola Mundo";
     }
 }
