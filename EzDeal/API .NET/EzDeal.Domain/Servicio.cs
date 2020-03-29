650c5c8761a9d069f409b112b2ac9218eedcf075
Cesar <u201710947@upc.edu.pe>
2020-03-28 19:49:26 -0500

    .NET en proceso

---
+++ EzDeal/API .NET/EzDeal.Domain/Servicio.cs
@@ -0,0 +1,16 @@
+namespace EzDeal.Domain
+{
+    public class Servicio
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Valor_Servicio { get; set; }
+        public bool Esta_Habilitado { get; set; }
+        public float Valoracion { get; set; }
+        public int? AnuncianteId { get; set; }
+        public int? ServicioId { get; set; }
+        public virtual Usuario Anunciante { get; set; }
+        public virtual TipoServicio TipoServicio { get; set; }
+    }
+}
---
+++ EzDeal/API .NET/EzDeal.Domain/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EzDeal.Domain
+{
+    public class Usuario
+    {
+        public int Id {get; set;}
+        public char Rol {get; set;}
+        public string Email {get; set;}
+        public string Contraseña {get; set;}
+        public string Nombres {get; set;}
+        public string Apellidos {get; set;}
+        public string Url_Contacto {get; set;}
+        public string TelefonoFijo {get; set;}
+        public string Celular {get;set;}
+        public string Distrito {get; set;}
+        public string Direccion {get; set;}
+        public string Provincia {get; set;}
+
+    }
+}
