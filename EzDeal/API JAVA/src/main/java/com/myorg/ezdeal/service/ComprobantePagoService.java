721217a7d6d312ff2d9ff050687948a2ecf246c7
Juanelv <slayz08@gmail.com>
2020-04-24 01:02:51 -0500

    se añadio post de comprobante de pago.

--- EzDeal/API JAVA/src/main/java/com/myorg/ezdeal/service/ComprobantePagoService.java
+++ EzDeal/API JAVA/src/main/java/com/myorg/ezdeal/service/ComprobantePagoService.java
@@ -1,4 +1,13 @@
 package com.myorg.ezdeal.service;
 
+import com.myorg.ezdeal.models.ComprobantePago;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public interface ComprobantePagoService {
+
+    ComprobantePago insertarComprobante(Map<String,String> body) throws  Exception;
+
 }
