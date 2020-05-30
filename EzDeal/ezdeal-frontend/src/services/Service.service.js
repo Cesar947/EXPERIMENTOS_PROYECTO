580ac837c1dd7620d5b590bc212b9ffb07586fc1
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-05-30 00:16:25 -0500

    Se añadio solicitar cita

--- EzDeal/ezdeal-frontend/src/services/Service.service.js
+++ EzDeal/ezdeal-frontend/src/services/Service.service.js
@@ -6,21 +6,21 @@ class ServicePublication {
 
     submitService(anuncianteId, tipoServicioId, servicio) {
         console.log(servicio);
-        return Axios.post(`${environment.api}/servicios?tipoServicioId=${tipoServicioId}&anuncianteId=${anuncianteId}`, servicio)
+        return Axios.post(`${environment.api}/servicios?tipoServicioId=${tipoServicioId}&anuncianteId=${anuncianteId}`, servicio, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
     }
 
     async getServiceDetail(id) {
 
 
         console.log(environment.token);
-        return await Axios.get(`${environment.api}/servicios/${id}`)
+        return await Axios.get(`${environment.api}/servicios/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
     }
 
     async publicarSolicitud(clienteId, servicioId, body) {
-        return await Axios.post(`${environment.api}/solicitudes?clienteId=${clienteId}&servicioId=${servicioId}`, { body })
+        return await Axios.post(`${environment.api}/solicitudes?clienteId=${clienteId}&servicioId=${servicioId}`, body)
     }
 
-    async getMyServices(anuncianteId){
+    async getMyServices(anuncianteId) {
         return await Axios.get(`${environment.api}/servicios/${anuncianteId}`)
     }
 
--- EzDeal/ezdeal-frontend/src/services/solicitud.service.js
+++ EzDeal/ezdeal-frontend/src/services/solicitud.service.js
@@ -1,11 +1,14 @@
 import Axios from "axios"
 import { environment } from "../environment/environment"
 
-class SolicitudService{
+class SolicitudService {
 
-    solicitar(servicioId,clienteId,solicitud){
+    solicitar(servicioId, clienteId, solicitud) {
         //Aquí también le quitaremos las llaves a solicitud?
-        return Axios.post(`${environment}/solicitud?servicioId=${servicioId}&clienteId=${clienteId}`,{solicitud})
+        return Axios.post(`${environment.api}/solicitudes?clienteId=${clienteId}&servicioId=${servicioId}`, solicitud, {
+            headers:
+                { Authorization: `Bearer ${localStorage.getItem("token")}` }
+        })
     }
 }
 
