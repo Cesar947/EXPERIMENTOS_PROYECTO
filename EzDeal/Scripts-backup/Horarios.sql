0c2d4b93b65ee8d892bb914634c001149ac228e9
Cesar947 <u201710947@upc.edu.pe>
2020-07-03 23:25:42 -0500

    horarios, geckodriver, navbar

---
+++ EzDeal/Scripts-backup/Horarios.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: select * from horario
+LIMIT 0, 1000
+
+-- Date: 2020-07-03 23:25
+*/
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (1,'Lunes','09:30:30','21:45:30',1);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (2,'Martes','09:30:30','21:45:30',1);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (3,'Miercoles','09:30:30','21:45:30',1);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (4,'Jueves','09:30:30','21:45:30',1);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (5,'Viernes','09:30:30','21:45:30',1);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (6,'Lunes','09:30:30','21:45:30',2);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (7,'Martes','09:30:30','21:45:30',2);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (8,'Miercoles','09:30:30','21:45:30',2);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (9,'Jueves','09:30:30','21:45:30',2);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (10,'Viernes','09:30:30','21:45:30',2);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (11,'Lunes','09:30:30','21:45:30',3);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (12,'Martes','09:30:30','21:45:30',3);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (13,'Miercoles','09:30:30','21:45:30',3);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (14,'Jueves','09:30:30','21:45:30',3);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (15,'Viernes','09:30:30','21:45:30',3);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (16,'Lunes','09:30:30','21:45:30',4);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (17,'Martes','09:30:30','21:45:30',4);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (18,'Miercoles','09:30:30','21:45:30',4);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (19,'Jueves','09:30:30','21:45:30',4);
+INSERT INTO `` (`horario_id`,`dia`,`hora_apertura`,`hora_cierre`,`servicio_id`) VALUES (20,'Viernes','09:30:30','21:45:30',4);
